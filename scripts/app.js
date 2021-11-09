94e80a220ab0ee23c0ffb98e8ebf4944d33eba33
unknown <gabrieljimenez79@hotmail.com>
2021-11-09 15:50:28 +0100

    NastanDar

--- scripts/app.js
+++ scripts/app.js
@@ -1,52 +1,28 @@
-window.addEventListener("load", function () {
-  menuBtn.addEventListener("click", function () {
-    toggleMenu();
-  });
-  for (let i = 0; i < links.length; i++) {
-    links[i].addEventListener("click", function () {
-      toggleMenu();
-    });
-  }
-});
-
 //service-modal code
-const services = [
-  {
-    name: "Frontend development",
-    content: "Wooohooo",
-    picture: "imgone.jpg",
-  },
-  {
-    name: "Backend development",
-    content: "Wooohooo2",
-    picture: "imgtwo.jpg",
-  },
-  {
-    name: "UX design",
-    content: "Wooohooo3",
-    picture: "imgthree.jpg",
-  },
-  {
-    name: "Coaching",
-    content: "Wooohooo4",
-    picture: "imgfour.jpg",
-  },
-];
-
+//the diffrent developers cards becomes developers
+const devs = document.getElementsByClassName("card");
+// when you press, close the modual
 const closeServiceModal = () => {
   document.getElementById("service-modal-wrapper").style.display = "none";
 };
 
+/*When you press,
+call on all "service-cards" and store them in "cards"
+depending on the cards you press and its index open it but...
+*/
 const openServiceModal = () => {
   const cards = document.getElementsByClassName("service-card");
   for (let i = 0; i < cards.length; i++) {
     cards[i].addEventListener("click", () => {
       document.getElementById("service-modal-wrapper").style.display = "flex";
-      for (let j = 0; j < services.length; j++) {
-        if (i === j) {
-          document.getElementById(
-            "service-modal-content"
-          ).innerHTML = `<h2>${services[j].name}</h2><p>${services[j].content}</p><img src="./media/img/${services[j].picture}">`;
+
+      /*...Depending on what id the card has display it in "service-modal-wrapper" IF it MATCHES with the id of the developer, and any remaning dev that is displayed will set to none*/
+      for (let j = 0; j < devs.length; j++) {
+        if (cards[i].id === devs[j].id) {
+          console.log(devs[j]);
+          devs[j].style.display = "block";
+        } else {
+          devs[j].style.display = "none";
         }
       }
     });
@@ -55,87 +31,40 @@ const openServiceModal = () => {
 
 window.addEventListener("load", openServiceModal);
 
-//Image-slider code
+// Get the modal
+const modal = document.getElementById("myModal");
 
-const images = [
-  {
-    name: "imgone.jpg",
-  },
-  {
-    name: "imgtwo.jpg",
-  },
-  {
-    name: "imgthree.jpg",
-  },
-  {
-    name: "imgfour.jpg",
-  },
-  {
-    name: "imgfive.jpg",
-  },
-  {
-    name: "imgsix.jpg",
-  },
-];
+// Retrieving the image and then we insert it to our modal
 
-const setMainImage = (src) => {
-  document.getElementById("main-image").setAttribute("src", src);
-  setActiveThumbNail();
-};
+const modalImg = document.getElementById("img01");
+const captionText = document.getElementById("caption");
+let lastClicked = null;
+// Here we settle a click function for our large/small image and the modal.
+function klickar(largeImage, miniBild) {
+  const bild = document.getElementById(largeImage);
+  const smallImage = document.getElementById(miniBild);
+  const modal = document.getElementById("myModal");
+  const modalImgOne = document.getElementById("img01");
 
-const setActiveThumbNail = () => {
-  const thumbs = document.getElementsByClassName("thumbnail");
-  for (let i = 0; i < thumbs.length; i++) {
-    if (thumbs[i].src === document.getElementById("main-image").src) {
-      thumbs[i].style.border = "2px solid red";
-    } else {
-      thumbs[i].style.border = "0px";
-    }
+  // This if statement checks if there has been a previously clicked image.
+  // When there has been no pictures selected (we just arrived to the page) then it's null.
+  // If picture is not equal to null then our remove function will run.
+  if (lastClicked != null) {
+    lastClicked.classList.remove("modal-section");
+    //console.log("Here");
   }
-};
+  // here we set the lastClicked image to the now clicked image
+  lastClicked = smallImage;
 
-const prevImage = () => {
-  const thumbs = document.getElementsByClassName("thumbnail");
-  for (let i = 0; i < thumbs.length; i++) {
-    if (
-      thumbs[i].src === document.getElementById("main-image").src &&
-      i !== 0
-    ) {
-      document
-        .getElementById("main-image")
-        .setAttribute("src", thumbs[(i -= 1)].src);
-      setActiveThumbNail();
-    }
-  }
-};
+  // here we apply the css rule to our new lastClicked image.
+  smallImage.classList.add("modal-section");
+  modal.style.display = "block";
+  console.log(modalImgOne);
+  modalImgOne.src = bild.src;
 
-const nextImage = () => {
-  const thumbs = document.getElementsByClassName("thumbnail");
-  for (let i = 0; i < thumbs.length; i++) {
-    if (
-      thumbs[i].src === document.getElementById("main-image").src &&
-      i !== thumbs.length - 1
-    ) {
-      document
-        .getElementById("main-image")
-        .setAttribute("src", thumbs[(i += 1)].src);
-      setActiveThumbNail();
-    }
-  }
-};
-
-window.addEventListener("load", () => {
-  document
-    .getElementById("main-image")
-    .setAttribute("src", `./media/img/${images[0].name}`);
-  document.getElementById("thumbnails-wrapper").innerHTML = images
-    .map(
-      (img) =>
-        `<img src="./media/img/${img.name}" class="thumbnail" onclick="setMainImage(this.src)">`
-    )
-    .join("");
-
-  setActiveThumbNail();
-  document.getElementById("prev-btn").addEventListener("click", prevImage);
-  document.getElementById("next-btn").addEventListener("click", nextImage);
-});
+  // here we set a simple function that closes our modal with span element X when clicked and turns the display to "none"
+  const span = document.getElementsByClassName("close")[0];
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+}
